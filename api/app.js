93e3e9b758eeade07084085df8dac430e9c5d63b
VikiMaiselman <maiselmanwikki@gmail.com>
2024-02-26 23:32:03 +0200

    starter files server

---
+++ api/app.js
@@ -0,0 +1,64 @@
+import "dotenv/config";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
+
+const app = express();
+const port = 3008;
+
+// CORS (development stage only)
+app.use(
+  cors({
+    origin: "http://localhost:3000", // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
+  })
+);
+app.use("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.options("*", cors());
+
+// Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Authentication
+app.use(
+  session({
+    secret: process.env.PASSPORT_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+import { posts } from "./posts/postRoute.js";
+
+app.use("/posts", posts);
+
+// Listening to incoming requests
+app.listen(port, () => console.log(`Server's up. Listening on port ${port}`));
+
+// DB connection close
+process.on("SIGINT", async () => {
+  try {
+    await mongoose.connection.close();
+    console.log(
+      "Mongoose connection is disconnected due to application termination"
+    );
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+  }
+});
